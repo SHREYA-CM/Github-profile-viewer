112600c2b93dc0f5a437ca756eb227a224f4a944
Shreya <tumhara.email@example.com>
2025-10-28 13:06:25 +0530

    fix: vite base path for vercel build

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './', // 👈 add this line
+  base: './', // ✅ Important for Vercel
 })
